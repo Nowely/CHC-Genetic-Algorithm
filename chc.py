ae3553b74e2cea9d7cbf373fbfdd108ac4941e95
Ruslan Ilyin <rilyin@optimajet.com>
2021-06-09 19:54:27 +0300

    Add functionality

--- chc.py
+++ chc.py
@@ -12,45 +12,84 @@
 
 # m - кол-во характеристик
 # n - кол-во доступных активностей в пуле
+import random
 
 import numpy as np
 
+
 class Individual:
- """ Класс одного индивида в популяции"""
- def __init__(self, m, n, fitness_function):
-  # Потенциальное решение (геном) - первый раз задается случайно
-  self.genome = np.random.randint(0, 2, (m, n), dtype=bool)
-  # Приспособленность индивида
-  self.fitness = 0
-  # Передача функции приспособленности (целевая функция)
-  self.fitness_function = fitness_function
+    """ Класс одного индивида в популяции"""
 
- def calculate_fitness(self):
-  """ Функция для пересчета значения приспособленности индивида"""
-  self.fitness = self.fitness_function(self.genome)
+    def __init__(self, m, n, fitness_function):
+        # Потенциальное решение (геном) - первый раз задается случайно
+        self.genome = np.random.randint(0, 2, (m, n), dtype=bool)
+        print(self.genome)
+        # Приспособленность индивида
+        self.fitness = 0
+        # Передача функции приспособленности (целевая функция)
+        self.fitness_function = fitness_function
+        # Считаем приспособленность индивида
+        self.calculate_fitness()
 
- def cataclysm_mutation(self):
-  """ Функция для мутации индивида, при которой меняется треть генов"""
-  # TODO
+    def calculate_fitness(self):
+        """ Функция для пересчета значения приспособленности индивида"""
+        self.fitness = self.fitness_function(self.genome)
+
+    def mutate_cataclysm(self):
+        """ Функция для мутации индивида, при которой меняется примерно треть генов"""
+        for i in range(self.genome.shape[0]):
+            for j in range(self.genome.shape[1]):
+                if random.random() <= 1 / 3:
+                    self.genome[i, j] = not (self.genome[i, j])
+        print(self.genome)
 
 
 class Genetic:
- """ Класс, отвечающий за реализацию генетического алгоритма"""
+    """ Класс, отвечающий за реализацию генетического алгоритма"""
 
- def __init__(self,
-              numberOfIndividums,
-              crossoverRate,
-              mutationSteps,
-              chanceMutations,
-              numberLives,
-              function,
-              start,
-              end):
+    def __init__(self,
+                 individuals_number,
+                 fitness_function,
+                 top_rate,
+                 m,
+                 n,
+                 delta
+                 ):
+        # Размер популяции
+        self.individuals_number = individuals_number
+        # Целевая функция
+        self.fitness_function = fitness_function
+        # Доля лучших, что дадут потомство и перейдут в следуюущую популяцию
+        self.top_rate = top_rate
+        # Размеры матрицы (генома)
+        self.m = m
+        self.n = n
+        # Пороговое значение для рассояния
+        self.delta = delta
 
+        # Лучшее значение, которое было в нашей популяции
+        self.best_fitness = None
+        # Геном лучшего значения
+        self.best_genome = None
 
- def crossing(self, parent1: Individual, parent2: Individual):
-  """ Функция для скрещивания двух родителей """
+    def crossing(self, parent_1: Individual, parent_2: Individual):
+        """ Функция для скрещивания двух родителей """
+        if self.calculate_distance(parent_1, parent_2) > self.delta:
+            # TODO половина несовпадающих значений от каждого родителя
+            temp = 2
 
+    def calculate_distance(self, parent_1: Individual, parent_2: Individual):
+        """ Функция для вычисления расстояния между родителями"""
+        result = 0
+        # TODO передавать и этот параметр? Проверить его влияние
+        d = 1
 
- def start_genetic(self):
-  """ Запуск отбора"""
+        for i in range(self.m):
+            for j in range(self.n):
+                if random.random() <= 1 / 3:
+                    result += abs(parent_1.genome[i, j] - parent_2.genome[i, j]) ** d
+
+        return result / parent_1.genome.size
+
+    def start_genetic(self):
+        """ Запуск отбора"""
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import numpy as np
 
 # Press Shift+F10 to execute it or replace it with your code.
+from chc import Individual, Genetic
+
+
 def print_hi():
     print(np.random.randint(0, 2, (4, 5), dtype=bool))
     # print(np.zeros((2, 3), dtype=bool))
 
 
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi()
+    testChild = Individual(4, 5, None)
